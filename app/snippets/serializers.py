cf6e91bfd4191d0da5c9793d6b43f46d49fe332d
LeeHanYeong <dev@lhy.kr>
2018-07-24 17:04:16 +0900

    SnippetList의 perform_create() 재정의, request.user를 owner에 할당

--- app/snippets/serializers.py
+++ app/snippets/serializers.py
@@ -1,10 +1,24 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Snippet
 
+User = get_user_model()
+
 __all__ = (
+    'UserSerializer',
     'SnippetSerializer',
 )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'pk',
+            'username',
+            'snippets',
+        )
 
 
 class SnippetSerializer(serializers.ModelSerializer):
@@ -17,4 +31,5 @@ class SnippetSerializer(serializers.ModelSerializer):
             'linenos',
             'language',
             'style',
+            'owner',
         )
--- app/snippets/views/generic_cbv.py
+++ app/snippets/views/generic_cbv.py
@@ -1,11 +1,16 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
 
 from ..models import Snippet
-from ..serializers import SnippetSerializer
+from ..serializers import SnippetSerializer, UserSerializer
+
+User = get_user_model()
 
 __all__ = (
     'SnippetList',
     'SnippetDetail',
+    'UserList',
+    'UserDetail',
 )
 
 
@@ -13,7 +18,23 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        # SnippetSerializer로 전달받은 데이터에
+        # 'owner'항목에 self.request.user데이터를 추가한 후
+        # save() 호출, DB에 저장 및 인스턴스 반환
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
